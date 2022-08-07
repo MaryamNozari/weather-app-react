c53b84b3c27720deda73a7f5a2add234abcad658
MaryamNozari <103874176+MaryamNozari@users.noreply.github.com>
2022-08-07 13:27:33 +0430

    remove extra files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
 import "./App.css";
 import Search from "./Search";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        {/* <h1>Hello World!!!</h1> */}
-        <Search />
-        {/* <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        > */}
-        {/* Learn React
-        </a> */}
-      </header>
+      <Search />
+
       <a
         href="https://github.com/MaryamNozari/weather-app-react"
         alt="github repository"
@@ -28,9 +14,6 @@ function App() {
       >
         github repository link
       </a>
-      {/* <h1>it is ok</h1> */}
     </div>
   );
 }
-
-export default App;
